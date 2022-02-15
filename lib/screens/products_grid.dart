1e192d1267a209321032b211a0ea12fdd8ffeb4b
zeenayouhan <zinayouhan33@gmail.com>
2022-02-15 12:07:20 +0530

    changenotifierprovider.value

--- lib/screens/products_grid.dart
+++ lib/screens/products_grid.dart
@@ -17,8 +17,8 @@ class ProductsGrid extends StatelessWidget {
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
         ),
-        itemBuilder: (ctx, i) => ChangeNotifierProvider(
-              create: (c) => products[i],
+        itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+              value: products[i],
               child: ProductItem(
                   // products[i].id, products[i].title, products[i].imageUrl),
                   ),
