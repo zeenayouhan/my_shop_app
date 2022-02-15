44db23937821b11d4dd6ee20f317bb37ea671a84
zeenayouhan <zinayouhan33@gmail.com>
2022-02-15 07:39:11 +0530

    added products

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop_app/screens/products_overview_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: ProductsOverViewScreen(),
     );
   }
 }
