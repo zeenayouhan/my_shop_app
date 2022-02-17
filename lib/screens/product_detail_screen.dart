827b491eecfb65c206d0b07860b0df329035a672
zeenayouhan <zinayouhan33@gmail.com>
2022-02-17 07:21:14 +0530

    show description of the product

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -19,6 +19,41 @@ class PruductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProducts.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProducts.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                '\$${loadedProducts.price}',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              loadedProducts.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
