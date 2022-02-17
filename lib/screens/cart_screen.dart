0ddddb5bc36834eaa491918629e0a5c15b4ec48e
zeenayouhan <zinayouhan33@gmail.com>
2022-02-17 07:14:09 +0530

    remove item

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_shop_app/providers/cart.dart';
 import 'package:provider/provider.dart';
+import 'package:my_shop_app/widgets/cart_item.dart' as ci;
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -41,7 +42,20 @@ class CartScreen extends StatelessWidget {
                 ],
               ),
             ),
-          )
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: (ctx, i) => ci.CartItem(
+                id: cart.items.values.toList()[i].id,
+                productId: cart.items.keys.toList()[i],
+                title: cart.items.values.toList()[i].title,
+                quantity: cart.items.values.toList()[i].quantity,
+                price: cart.items.values.toList()[i].price),
+            itemCount: cart.itemCount,
+          ))
         ],
       ),
     );
